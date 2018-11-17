2b1f45df962023f15d44cf78ff1286e2c6b7a021
Robbie Mackay <rm@robbiemackay.com>
2018-11-17 14:40:22 +1300

    feat: Add price per items input and update tests

--- src/CalculatorForm.js
+++ src/CalculatorForm.js
@@ -6,10 +6,12 @@ class CalculatorForm extends Component {
     super(props);
     this.state = {
       numberOfItems: '',
+      pricePerItem: '',
       lastSubmission: {}
     };
 
     this.handleNumberOfItemsChange = this.handleNumberOfItemsChange.bind(this);
+    this.handlePricePerItemChange = this.handlePricePerItemChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -17,12 +19,17 @@ class CalculatorForm extends Component {
     this.setState({numberOfItems: event.target.value});
   }
 
+  handlePricePerItemChange(event) {
+    this.setState({pricePerItem: event.target.value});
+  }
+
   handleSubmit(event) {
     event.preventDefault();
 
     this.setState({
       lastSubmission: {
-        numberOfItems: this.state.numberOfItems
+        numberOfItems: this.state.numberOfItems,
+        pricePerItem: this.state.pricePerItem
       }
     });
   }
@@ -32,16 +39,25 @@ class CalculatorForm extends Component {
       <div className="CalculatorForm">
         <h2>Purchase Form</h2>
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Number of items:
-            <input type="text" name="numberOfItems" value={this.state.numberOfItems} onChange={this.handleNumberOfItemsChange} />
-          </label>
+          <div>
+            <label>
+              Number of items:
+              <input type="text" name="numberOfItems" value={this.state.numberOfItems} onChange={this.handleNumberOfItemsChange} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Price per item:
+              <input type="text" name="pricePerItem" value={this.state.pricePerItem} onChange={this.handlePricePerItemChange} />
+            </label>
+          </div>
           <input type="submit" value="Submit"/>
         </form>
 
         <h3>Results:</h3>
         <p>
-          Number of items: {this.state.lastSubmission.numberOfItems}
+          Number of items: {this.state.lastSubmission.numberOfItems}<br />
+          Price per item: ${this.state.lastSubmission.pricePerItem}
         </p>
       </div>
     );
--- src/CalculatorForm.test.js
+++ src/CalculatorForm.test.js
@@ -17,8 +17,15 @@ it('saves numberOfItems to state and outputs it', () => {
   expect(tree).toMatchSnapshot();
 
   // manually trigger state changes
-  let input = component.root.findByProps({name : 'numberOfItems'});
-  input.props.onChange({ target: { value: '100' } });
+  // Set number of items
+  let itemInput = component.root.findByProps({name : 'numberOfItems'});
+  itemInput.props.onChange({ target: { value: '100' } });
+
+  // Set price per item
+  let priceInput = component.root.findByProps({name : 'pricePerItem'});
+  priceInput.props.onChange({ target: { value: '200' } });
+
+  // Submit the form
   let submit = component.root.findByType('form');
   submit.props.onSubmit({
   	preventDefault : () => {}
