d0793efc9fe777be986c1e7a7af3f2215f2e0ea0
Robbie Mackay <rm@robbiemackay.com>
2018-11-19 10:21:52 +1300

    feat: add state code input to calculatorForm

--- src/CalculatorForm.test.js
+++ src/CalculatorForm.test.js
@@ -26,6 +26,10 @@ it('saves numberOfItems to state and outputs it', () => {
   let priceInput = component.root.findByProps({name : 'pricePerItem'});
   priceInput.props.onChange({ target: { value: '200' } });
 
+  // Set state code
+  let stateInput = component.root.findByProps({name : 'stateCode'});
+  stateInput.props.onChange({ target: { value: 'UT' } });
+
   // Submit the form
   let submit = component.root.findByType('form');
   submit.props.onSubmit({
@@ -36,6 +40,7 @@ it('saves numberOfItems to state and outputs it', () => {
   expect(submitHandler.mock.calls[0]).toEqual([{
     numberOfItems: "100",
     pricePerItem: "200",
+    stateCode: 'UT'
   }]);
 
   // re-rendering
